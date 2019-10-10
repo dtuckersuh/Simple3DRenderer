a79f8ecde6b5abb47414b17bfb3e13377f87e097
Daniel Suh <suh977@gmail.com>
2019-10-10 00:33:15 -0700

    Added shading and sphere

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -3,7 +3,6 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +28,6 @@ public class DemoViewer {
 
 		// panel to display render results
 		JPanel renderPanel = new JPanel() {
-			@SuppressWarnings("unchecked")
 			@Override
 			public void paintComponent(Graphics g) {
 				Graphics2D g2 = (Graphics2D) g;
@@ -51,16 +49,9 @@ public class DemoViewer {
 				tris.add(new Triangle(new Vertex(-100, 100, -100), new Vertex(100, -100, -100),
 						new Vertex(-100, -100, 100), Color.BLUE));
 
-//				g2.translate(getWidth() / 2, getHeight() / 2);
-//				g2.setColor(Color.WHITE);
-//				for (Triangle t : tris) {
-//					java.awt.geom.Path2D path = new Path2D.Double();
-//					path.moveTo(t.v1.x, t.v1.y);
-//					path.lineTo(t.v2.x, t.v2.y);
-//					path.lineTo(t.v3.x, t.v3.y);
-//					path.closePath();
-//					g2.draw(path);
-//				}
+				for (int i = 0; i < 4; i++) {
+					tris = inflate(tris);
+				}
 
 				// XZ rotation matrix
 				double heading = Math.toRadians(headingSlider.getValue());
@@ -76,21 +67,6 @@ public class DemoViewer {
 						-Math.sin(pitch), Math.cos(pitch) });
 				transform = headingTransform.multiply(pitchTransform);
 
-				// Creates wireframe of shape
-//				g2.translate(getWidth() / 2, getHeight() / 2);
-//				g2.setColor(Color.WHITE);
-//				for (Triangle t : tris) {
-//					Vertex v1 = transform.tranform(t.v1);
-//					Vertex v2 = transform.tranform(t.v2);
-//					Vertex v3 = transform.tranform(t.v3);
-//					Path2D path = new Path2D.Double();
-//					path.moveTo(v1.x, v1.y);
-//					path.lineTo(v2.x, v2.y);
-//					path.lineTo(v3.x, v3.y);
-//					path.closePath();
-//					g2.draw(path);
-//				}
-
 				// Rasterizes triangle - converts it to list of pixels on screen it occupies
 				BufferedImage img = new BufferedImage(getWidth(), getHeight(),
 						java.awt.image.BufferedImage.TYPE_INT_ARGB);
@@ -105,6 +81,17 @@ public class DemoViewer {
 					Vertex v1 = transform.tranform(t.v1);
 					Vertex v2 = transform.tranform(t.v2);
 					Vertex v3 = transform.tranform(t.v3);
+
+					Vertex ab = new Vertex(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
+					Vertex ac = new Vertex(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z);
+					Vertex norm = new Vertex(ab.y * ac.z - ab.z * ac.y, ab.z * ac.x - ab.x * ac.z,
+							ab.x * ac.y - ab.y * ac.x);
+					double normalLength = Math.sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z);
+					norm.x /= normalLength;
+					norm.y /= normalLength;
+					norm.z /= normalLength;
+
+					double angleCos = Math.abs(norm.z);
 
 					// Since we are not using Graphics2D
 					// We have to translate manually
@@ -127,11 +114,12 @@ public class DemoViewer {
 							double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
 							double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
 							double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+							// For each rasterized pixel
 							if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
 								double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
 								int zIndex = y * img.getWidth() + x;
 								if (zBuffer[zIndex] < depth) {
-									img.setRGB(x, y, t.color.getRGB());
+									img.setRGB(x, y, getShade(t.color, angleCos).getRGB());
 									zBuffer[zIndex] = depth;
 								}
 							}
@@ -166,4 +154,42 @@ public class DemoViewer {
 			this.color = color;
 		}
 	}
+
+	// Converts color from scaled to linear format, applies shade,
+	// then converts back to scaled format
+	public static Color getShade(Color color, double shade) {
+		double redLinear = Math.pow(color.getRed(), 2.4) * shade;
+		double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
+		double blueLinear = Math.pow(color.getBlue(), 2.4) * shade;
+
+		int red = (int) Math.pow(redLinear, 1 / 2.4);
+		int green = (int) Math.pow(greenLinear, 1 / 2.4);
+		int blue = (int) Math.pow(blueLinear, 1 / 2.4);
+
+		return new Color(red, green, blue);
+	}
+
+	// Sphere approximation
+	public static List<Triangle> inflate(List<Triangle> tris) {
+		List<Triangle> result = new ArrayList<>();
+		for (Triangle t : tris) {
+			Vertex m1 = new Vertex((t.v1.x + t.v2.x) / 2, (t.v1.y + t.v2.y) / 2, (t.v1.z + t.v2.z) / 2);
+			Vertex m2 = new Vertex((t.v2.x + t.v3.x) / 2, (t.v2.y + t.v3.y) / 2, (t.v2.z + t.v3.z) / 2);
+			Vertex m3 = new Vertex((t.v1.x + t.v3.x) / 2, (t.v1.y + t.v3.y) / 2, (t.v1.z + t.v3.z) / 2);
+			result.add(new Triangle(t.v1, m1, m3, t.color));
+			result.add(new Triangle(t.v2, m1, m2, t.color));
+			result.add(new Triangle(t.v3, m2, m3, t.color));
+			result.add(new Triangle(m1, m2, m3, t.color));
+		}
+		for (Triangle t : result) {
+			for (Vertex v : new Vertex[] { t.v1, t.v2, t.v3 }) {
+				double l = Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z) / Math.sqrt(30000);
+				v.x /= l;
+				v.y /= l;
+				v.z /= l;
+			}
+		}
+		return result;
+	}
+
 }
